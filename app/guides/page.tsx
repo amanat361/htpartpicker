d0eb9eef600377b9ca764140c204d653a729a965
Samin Amanat <amanat361@gmail.com>
2024-01-18 21:12:16 -0800

    added text to guides page for construction

--- app/guides/page.tsx
+++ app/guides/page.tsx
@@ -1,3 +1,14 @@
+import { Text, TextLink } from "@components/text";
+
 export default function GuidesPage() {
-  return <div>Guides are going to go here.</div>;
+  return (
+    <div>
+      <Text>
+        Hey there! If you are seeing this, you probably are someone that knows a
+        thing or two about Home Theaters. Checkout{" "}
+        <TextLink href="/build">the build page</TextLink> and send me any and
+        all feedback. Cheers!
+      </Text>
+    </div>
+  );
 }
