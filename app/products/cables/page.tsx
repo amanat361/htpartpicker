afb2fb5fb267a243d60f0819a02cf121a7811d5f
Samin Amanat <amanat361@gmail.com>
2024-01-19 01:05:29 -0800

    added temp product placeholders

---
+++ app/products/cables/page.tsx
@@ -0,0 +1,14 @@
+import { Text, TextLink } from "@components/text";
+
+export default function TempPage() {
+  return (
+    <div>
+      <Text>
+        Hey there! If you are seeing this, you probably are someone that knows a
+        thing or two about Home Theaters. Checkout{" "}
+        <TextLink href="/build">the build page</TextLink> and send me any and
+        all feedback. Cheers!
+      </Text>
+    </div>
+  );
+}
