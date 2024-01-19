b6224ede3adcdc578b4d081297a3ef40237dd007
Samin Amanat <amanat361@gmail.com>
2024-01-18 20:08:08 -0800

    updated home page for construction

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Text, TextLink } from "@components/text";
+
 export default function Home() {
   return (
     <div>
-      Hello World.{" "}
+      <Text>Hey there! If you are seeing this, you probably are someone that knows a thing or two about Home Theaters. Checkout <TextLink href="/build">the build page</TextLink> and send me any and all feedback. Cheers!</Text>
     </div>
   );
 }
