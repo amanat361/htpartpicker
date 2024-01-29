c2b16b2d3c280e8f2bb179db8b5a607a8bb91f89
Samin Amanat <amanat361@gmail.com>
2024-01-28 17:43:22 -0800

    web scraping communicates products back to frontend

--- app/admin/scrape/page.tsx
+++ app/admin/scrape/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import {
@@ -8,32 +9,100 @@ import {
   Label,
   Legend,
 } from "@components/fieldset";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@components/table";
 import { Button } from "@components/button";
 import { Input } from "@components/input";
 import { startScraping } from "@utils/productScraper";
 import { useFormStatus } from "react-dom";
-import { Text } from "@components/text";
+import { useFormState } from "react-dom";
+import { Text, TextLink } from "@components/text";
+import { FormState } from "@utils/productScraper";
 
-export default function ScrapePage() {
+const initialState = {
+  products: [],
+  message: "",
+  hasError: false,
+} as FormState;
+
+function SubmitButton() {
   const { pending } = useFormStatus();
   return (
-    <form action={startScraping} className="space-y-4">
-      <Fieldset>
-        <Legend>Scrape a URL</Legend>
-        <Text>
-          This form will allow you to scrape a URL and return the data. This
-          will be one of the places where this product is sold.
-        </Text>
-        <FieldGroup>
-          <Field>
-            <Label>URL</Label>
-            <Input name="url"/>
-          </Field>
-          <Button type="submit" disabled={pending}>
-            Scrape
-          </Button>
-        </FieldGroup>
-      </Fieldset>
-    </form>
+    <Button
+      type="submit"
+      disabled={pending}
+    >
+      {pending ? "Loading..." : "Scrape"}
+    </Button>
+  );
+}
+
+export default function ScrapePage() {
+  const [state, formAction] = useFormState(startScraping, initialState);
+
+  return (
+    <div className="max-w-6xl space-y-8">
+      <form action={formAction} className="space-y-4">
+        <Fieldset>
+          <Legend>Scrape a URL</Legend>
+          <Text>
+            This form will allow you to scrape a URL and return the data. This
+            will be one of the places where this product is sold.
+          </Text>
+          <FieldGroup>
+            <Field>
+              <Label>URL</Label>
+              <Input name="url" />
+            </Field>
+            <SubmitButton />
+          </FieldGroup>
+        </Fieldset>
+      </form>
+      {state.hasError && <Text>{state.message}</Text>}
+      {state.products.length > 0 && (
+        <Table
+          dense
+          bleed
+          className="[--gutter:theme(spacing.6)] sm:[--gutter:theme(spacing.8)]"
+        >
+          <TableHead>
+            <TableRow>
+              <TableHeader>Image</TableHeader>
+              <TableHeader>Title</TableHeader>
+              <TableHeader>Brand</TableHeader>
+              <TableHeader>Price</TableHeader>
+              <TableHeader>URL</TableHeader>
+              <TableHeader>Description</TableHeader>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {state.products.map((product) => (
+              <TableRow key={product.url}>
+                <TableCell>
+                  <img
+                    src={product.image}
+                    alt={product.title}
+                    className="w-12 h-12 rounded-xl object-cover"
+                  />
+                </TableCell>
+                <TableCell>{product.title}</TableCell>
+                <TableCell>{product.brand}</TableCell>
+                <TableCell>{product.price}</TableCell>
+                <TableCell>
+                  <TextLink href={product.url}>View on Crutchfield</TextLink>
+                </TableCell>
+                <TableCell>{product.description.length > 30 ? product.description.slice(0, 30) + "..." : product.description}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
+    </div>
   );
 }
--- app/utils/productScraper.ts
+++ app/utils/productScraper.ts
@@ -1,72 +1,143 @@
 "use server";
 
 import puppeteer from "puppeteer";
+import type { Page } from "puppeteer";
 
 export interface Product {
   url: string;
-  prodImage: string;
+  image: string;
   title: string;
   price: string;
+  brand: string;
+  description: string;
   highlights: string[];
 }
 
-async function findAllProductsOnPage(url: string) {
-  console.log("Now scraping: " + url)
+export interface FormState {
+  products: Product[];
+  message: string;
+  hasError: boolean;
+}
+
+async function getBrandList(url: string): Promise<string[]> {
   const browser = await puppeteer.launch({
-    headless: "new", // Opting into the new Headless mode
+    headless: "new",
   });
   const page = await browser.newPage();
   await page.goto(url);
-  // await page.waitForNetworkIdle({idleTime: 1000});
 
-  const products = await page.evaluate(() => {
-    const productBlocks = document.querySelectorAll(".product-block");
-    const products: Product[] = [];
-    productBlocks.forEach((productBlock) => {
+  // Scrape brand labels with 'for' attribute starting with 'Brand'
+  const brands = await page.evaluate(() => {
+    const brandLabels = Array.from(document.querySelectorAll('.form-check-label[for^="Brand"]')) as HTMLLabelElement[];
+    return brandLabels.map(label => label.textContent?.trim() || '');
+  });
+
+  await browser.close();
+  return brands;
+}
+
+async function findAllProductsOnPage(url: string): Promise<Product[]> {
+  const browser = await puppeteer.launch({
+    headless: "new",
+  });
+
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  // Function to scroll through the page
+  async function autoScroll(page: Page) {
+    await page.evaluate(async () => {
+      await new Promise<void>((resolve, reject) => {
+        var totalHeight = 0;
+        var distance = 100;
+        var timer = setInterval(() => {
+          var scrollHeight = document.body.scrollHeight;
+          window.scrollBy(0, distance);
+          totalHeight += distance;
+
+          if (totalHeight >= scrollHeight) {
+            clearInterval(timer);
+            resolve();
+          }
+        }, 100);
+      });
+    });
+  }
+
+  // Scroll through the page to trigger lazy loading
+  await autoScroll(page);
+
+  // Now scrape the products
+  const products: Product[] = await page.evaluate(() => {
+    const productBlocks = Array.from(
+      document.querySelectorAll(".product-block")
+    );
+    return productBlocks.map((productBlock) => {
       const product: Product = {
         url: "",
-        prodImage: "",
+        image: "",
         title: "",
         price: "",
+        brand: "",
+        description: "",
         highlights: [],
       };
+
       const a = productBlock.querySelector("a");
-      if (a) {
-        product.url = a.href;
-      }
-      // page.waitForSelector("img.prodImage", { visible: true });
-      const img = productBlock.querySelector("img.prodImage");
-      if (img) {
-        product.prodImage = img.getAttribute("src") || "";
-      }
+      product.url = a?.href || "";
+
+      const img = productBlock.querySelector(
+        "img.prodImage"
+      ) as HTMLImageElement;
+      product.image = img?.src || "";
+
       const h4 = productBlock.querySelector("h4.product-list-items-title");
-      if (h4) {
-        product.title = h4.textContent || "";
-      }
+      product.title = h4?.textContent || "";
+
+      const p = productBlock.querySelector("p.product-list-items-desc");
+      product.description = p?.textContent || "";
+
       const span = productBlock.querySelector("span.price");
-      if (span) {
-        product.price = span.textContent || "";
-      }
+      product.price = span?.textContent || "";
+
       const ul = productBlock.querySelector("ul.product-list-items-highlights");
       if (ul) {
         const lis = ul.querySelectorAll("li");
-        lis.forEach((li) => {
-          product.highlights.push(li.textContent || "");
-        });
+        product.highlights = Array.from(lis).map((li) => li.textContent || "");
       }
-      products.push(product);
+
+      return product;
     });
-    return products;
   });
 
   await browser.close();
   return products;
 }
 
-export async function startScraping(formData: FormData) {
+export async function startScraping(state: FormState, formData: FormData) {
   const rawFormData = Object.fromEntries(formData.entries());
   const url = rawFormData.url as string;
+  console.clear();
+  console.log("Scraping started...");
+  const brands = await getBrandList(url);
   const products = await findAllProductsOnPage(url);
-  console.log(products)
-  return products;
+  products.forEach((product) => {
+    const brand = brands.find((brand) => product.title.includes(brand));
+    product.brand = brand || "";
+  });
+  console.log("Scraping complete!");
+
+  if (products.length === 0) {
+    return {
+      hasError: true,
+      message: "No products found!",
+      products: [],
+    };
+  }
+
+  return {
+    hasError: false,
+    message: "Scraping complete!",
+    products,
+  };
 }
