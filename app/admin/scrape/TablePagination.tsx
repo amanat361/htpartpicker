2e55c994cc383a3d9f91bc96a87ab80c78320143
Samin Amanat <amanat361@gmail.com>
2024-02-10 14:43:20 -0800

    improved pagination range with gaps

--- app/admin/scrape/TablePagination.tsx
+++ app/admin/scrape/TablePagination.tsx
@@ -7,6 +7,24 @@ import {
   PaginationPrevious,
 } from "@components/pagination";
 
+function generatePageNumbers(currentPage: number, totalPages: number) {
+  const start = [1,2,3];
+  const end = [totalPages-2, totalPages-1, totalPages];
+  const middle = [currentPage-1, currentPage, currentPage+1];
+  const all = [...start, ...middle, ...end];
+
+  if (totalPages <= 6) {
+    return all;
+  }
+  if (currentPage < 3) {
+    return [...start, null, ...end];
+  }
+  if (currentPage > totalPages - 2) {
+    return [...start, null, ...end];
+  }
+  return [1, null, ...middle, null, totalPages];
+}
+
 export default function TablePagination({
   stateLength,
   page,
@@ -16,25 +34,31 @@ export default function TablePagination({
   page: number;
   itemsPerPage: number;
 }) {
+  const totalPages = Math.ceil(stateLength / itemsPerPage);
+  const pageNumbers = generatePageNumbers(page, totalPages);
+
   return (
     <Pagination>
       <PaginationPrevious href={page === 1 ? null : `?page=${page - 1}`} />
       <PaginationList>
-        {Array.from({ length: Math.ceil(stateLength / itemsPerPage) }).map(
-          (_, i) => {
-            const pageNumber = i + 1;
+        {pageNumbers.map((pageNumber, i) => {
+          if (pageNumber) {
             return (
-              <PaginationPage key={pageNumber} href={`?page=${pageNumber}`}>
-                {pageNumber as unknown as string}
+              <PaginationPage
+                key={i}
+                href={`?page=${pageNumber}`}
+                current={pageNumber === page}
+              >
+                {`${pageNumber}`}
               </PaginationPage>
             );
+          } else {
+            return <PaginationGap key={i} />;
           }
-        )}
+        })}
       </PaginationList>
       <PaginationNext
-        href={
-          stateLength <= page * itemsPerPage ? null : `?page=${page + 1}`
-        }
+        href={stateLength <= page * itemsPerPage ? null : `?page=${page + 1}`}
       />
     </Pagination>
   );
