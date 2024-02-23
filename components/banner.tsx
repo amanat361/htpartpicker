4d263293603c2ec0b73e20ca3ac2cc2cb9c356cc
Samin Amanat <amanat361@gmail.com>
2024-02-22 22:41:44 -0800

    added better looking commit status to home page

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,59 +1,164 @@
-"use client"
-import { XMarkIcon } from "@heroicons/react/20/solid";
+"use client";
+import {
+  XMarkIcon,
+  // PlusCircleIcon,
+  // MinusCircleIcon,
+} from "@heroicons/react/20/solid";
 import { useState } from "react";
 
-export default function Banner({
-  message,
-  url,
-}: {
-  message: string;
-  url: string;
-}) {
-  const [dismissed, setDismissed] = useState(false);
-  if (dismissed) return null;
+type CommitData = {
+  commit: {
+    message: string;
+  };
+  html_url: string;
+  stats: {
+    total: number;
+    additions: number;
+    deletions: number;
+  };
+};
+
+function MinusCircleIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <>
-      <div className="pointer-events-none fixed inset-x-0 bottom-0 sm:px-6 sm:pb-5 lg:px-8">
-        <div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-blue-800 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 1024 1024"
-            fill="white"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z"
-              transform="scale(64)"
-              fill="white"
-            />
-          </svg>
-          <p className="text-sm leading-6 text-white">
-            <a href={url} target="_blank">
-              <strong className="font-semibold">HT Part Picker Update</strong>
-              <svg
-                viewBox="0 0 2 2"
-                className="mx-2 inline h-0.5 w-0.5 fill-current"
-                aria-hidden="true"
-              >
-                <circle cx={1} cy={1} r={1} />
-              </svg>
-              {message}&nbsp;
-              <span aria-hidden="true">&rarr;</span>
-            </a>
-          </p>
-          <button
-            type="button"
-            className="-m-3 flex-none p-3 focus-visible:outline-offset-[-4px]"
-            onClick={() => setDismissed(true)}
-          >
-            <span className="sr-only">Dismiss</span>
-            <XMarkIcon className="h-5 w-5 text-white" aria-hidden="true" />
-          </button>
-        </div>
-      </div>
-    </>
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <path d="M8 12h8" />
+    </svg>
   );
 }
+
+function PlusCircleIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <path d="M8 12h8" />
+      <path d="M12 8v8" />
+    </svg>
+  );
+}
+
+export default function Banner({ data }: { data: CommitData }) {
+  const [dismissed, setDismissed] = useState(false);
+  if (dismissed) return null;
+
+  return (
+    <a href={data.html_url} target="_blank" className="flex items-center h-16 border border-black dark:border-white hover:cursor-pointer rounded-lg p-4 gap-4 fixed bottom-4 hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black transition-all group">
+      <div className="text-lg flex items-center gap-4 ">
+        <svg
+          width="30"
+          height="30"
+          viewBox="0 0 1024 1024"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z"
+            transform="scale(64)"
+            className="fill-black dark:fill-white group-hover:fill-white dark:group-hover:fill-black transition-all"
+          />
+        </svg>
+        <div>
+          <h3 className="font-semibold">HT Part Picker Update</h3>
+          <p className="text-sm text-gray-800 group-hover:text-gray-400 dark:text-gray-400 dark:group-hover:text-gray-800 transition-all">
+            {data.commit.message}
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center gap-2 text-emerald-700 group-hover:text-emerald-400 dark:text-emerald-400 dark:group-hover:text-emerald-700 transition-all">
+        <PlusCircleIcon className="w-6 h-6" />
+        <span className="text-md">{data.stats.additions} Additions</span>
+      </div>
+      <div className="flex items-center gap-2 text-rose-700 group-hover:text-rose-400 dark:text-rose-500 dark:group-hover:text-rose-700 transition-all">
+        <MinusCircleIcon className="w-6 h-6" />
+        <span className="text-md">{data.stats.deletions} Deletions</span>
+      </div>
+      <button
+        type="button"
+        className="p-2 focus-visible:outline-offset-[-4px]"
+        onClick={() => setDismissed(true)}
+      >
+        <span className="sr-only">Dismiss</span>
+        <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+      </button>
+    </a>
+  );
+}
+
+// return (
+//   <>
+//     <div className="pointer-events-none fixed inset-x-0 bottom-0 sm:px-6 sm:pb-5 lg:px-8">
+//       <div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-gray-100 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
+//         <svg
+//           width="20"
+//           height="20"
+//           viewBox="0 0 1024 1024"
+//           fill="black"
+//           xmlns="http://www.w3.org/2000/svg"
+//         >
+//           <path
+//             fill-rule="evenodd"
+//             clip-rule="evenodd"
+//             d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z"
+//             transform="scale(64)"
+//             fill="black"
+//           />
+//         </svg>
+//         <p className="text-sm leading-6 text-black">
+//           <a href={data.html_url} target="_blank">
+//             <div className="flex justify-center items-center gap-4">
+//               <strong className="font-semibold">HT Part Picker Update</strong>
+//               <svg
+//                 viewBox="0 0 2 2"
+//                 className="inline h-0.5 w-0.5 fill-current"
+//                 aria-hidden="true"
+//               >
+//                 <circle cx={1} cy={1} r={1} />
+//               </svg>
+//               {data.commit.message}
+//               <div className="flex justify-center items-center gap-1">
+//                 <PlusCircleIcon className="w-5 h-5 inline text-green-400" />
+//                 <span>{data.stats.additions}</span>
+//               </div>
+//               <div className="flex justify-center items-center gap-1">
+//                 <MinusCircleIcon className="w-5 h-5 inline text-red-400" />
+//                 <span>{data.stats.deletions}</span>
+//               </div>
+//               <span aria-hidden="true">&rarr;</span>
+//             </div>
+//           </a>
+//         </p>
+//         <button
+//           type="button"
+//           className="-m-3 flex-none p-3 focus-visible:outline-offset-[-4px]"
+//           onClick={() => setDismissed(true)}
+//         >
+//           <span className="sr-only">Dismiss</span>
+//           <XMarkIcon className="h-5 w-5 text-black" aria-hidden="true" />
+//         </button>
+//       </div>
+//     </div>
+//   </>
+// );
