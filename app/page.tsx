ef5ed1465fcc8d786a1fc82823d7daa0b586a61e
Samin Amanat <amanat361@gmail.com>
2024-02-22 21:17:02 -0800

    added recent commit to home page banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
 import Greeting from "@/components/construction";
+import Banner from "@/components/banner";
 
-export default function Home() {
-  return <Greeting />;
+const repository_url =
+  "https://api.github.com/repos/amanat361/htpartpicker/commits/dev?per_page=1";
+
+export default async function Home() {
+
+  const response = await fetch(repository_url);
+  const data = await response.json();
+  const message = data["commit"]["message"] || "";
+  const url = data["html_url"] || "";
+
+  return (
+    <>
+      <Greeting />
+      {message && url && <Banner message={message} url={url} />}
+    </>
+  );
 }
