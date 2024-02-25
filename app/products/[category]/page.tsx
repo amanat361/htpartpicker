ab8d1cd5c8b7fba82cc8cccea101724976d74014
Samin Amanat <amanat361@gmail.com>
2024-02-25 13:55:12 -0800

    display filtered products on dynamic routes for categories

--- app/products/[category]/page.tsx
+++ app/products/[category]/page.tsx
@@ -1,25 +1,41 @@
 import { getCategories } from "@/database/methods";
+import { getProducts } from "@/database/methods";
 import Greeting from "@/components/construction";
 
-export default function PageUnderConstruction({
+export default async function PageUnderConstruction({
   params,
 }: {
   params: { category: string };
 }) {
+  const products = await getProducts();
+  if (products.result.hasError) return <div>error</div>;
+  const filteredProducts = products.data.filter(
+    (product) => product.category === params.category
+  );
+
   return (
     <>
-      {params.category}
+      <h1 className="text-2xl">{params.category}</h1>
+      <div className="flex flex-col gap-4 w-full max-w-6xl">
+        {filteredProducts.map((product) => (
+          <div key={product.id} className="w-full flex gap-4 items-center">
+            <img
+              className="w-16 h-16"
+              src={product.image_url}
+              alt={product.name}
+            />
+            {product.name}
+          </div>
+        ))}
+      </div>
       <Greeting />
     </>
   );
 }
 
-
 export async function generateStaticParams() {
-  const {data, result} = await getCategories();
-  if (result.hasError) return [
-    { category: "error" },
-  ];
+  const { data, result } = await getCategories();
+  if (result.hasError) return [{ category: "error" }];
 
   return data.map((category) => ({
     category: category.name,
