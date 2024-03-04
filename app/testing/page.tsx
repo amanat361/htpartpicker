c4499b90bfd1d02dc60dacd896fa5fd85d23e57b
Samin Amanat <amanat361@gmail.com>
2024-03-04 00:49:43 +0000

    Add react-error-boundary package and error handling page

---
+++ app/testing/page.tsx
@@ -0,0 +1,11 @@
+import ButtonWithError from "./buttonwitherror";
+
+export default function TestingPage() {
+  return (
+    <div>
+      <h1>Testing Page</h1>
+      <p>Testing page content</p>
+      <ButtonWithError />
+    </div>
+  );
+}
