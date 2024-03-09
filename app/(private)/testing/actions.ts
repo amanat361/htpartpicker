f4c58197614f207e03b256bdcc57fbda40a82dd2
Samin Amanat <amanat361@gmail.com>
2024-03-09 03:00:52 +0000

    Refactor addProduct function to return a promise with resolved data or rejected error

--- app/(private)/testing/actions.ts
+++ app/(private)/testing/actions.ts
@@ -9,12 +9,30 @@ const ProductSchema = z.object({
   product_brand: z.string().min(1),
 });
 
+type Product = z.infer<typeof ProductSchema>;
+
 export async function addProduct(formData: FormData) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  try {
-    const data = ProductSchema.parse(Object.fromEntries(formData.entries()));
-    return data;
-  } catch (error) {
-    throw fromZodError(error as ZodError);
-  }
+
+  // try {
+  //   const data = ProductSchema.parse(Object.fromEntries(formData.entries()));
+  //   return data;
+  // } catch (error) {
+  //   return fromZodError(error as ZodError);
+  // }
+
+  // same thing as above but return a promise that resolves with the data or rejects with the error
+  return new Promise(
+    (
+      resolve: (data: Product) => void,
+      reject: (reason: string) => void
+    ) => {
+      const data = ProductSchema.safeParse(
+        Object.fromEntries(formData.entries())
+      );
+      if (!data.success)
+        reject(fromZodError(data.error).toString() + " (from promise)");
+      if (data.success) resolve(data.data);
+    }
+  );
 }
