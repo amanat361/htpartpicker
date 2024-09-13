a9bc33117fdc6298d55f4708be8c4b7c1ac5eb3b
amanat361 <amanat361@gmail.com>
2024-09-13 00:51:17 -0700

    chunked the fetches to batches of 500 products

--- app/(public)/foo/page.tsx
+++ app/(public)/foo/page.tsx
@@ -2,13 +2,36 @@ import { supabase } from "@/database/server";
 import { ClientSideProductGrid } from "./ProductGrid";
 
 export default async function NewProductsPage() {
-  const { data: products, error } = await supabase
+  const { data: products1, error: error1 } = await supabase
     .from("crutchfield")
-    .select("*");
+    .select("*")
+    .range(0,500);
 
-  if (!products || error) {
-    return <div>There was an error fetching products. {error.message}</div>;
+  const { data: products2, error: error2 } = await supabase
+    .from("crutchfield")
+    .select("*")
+    .range(500,1000);
+
+  const { data: products3, error: error3 } = await supabase
+    .from("crutchfield")
+    .select("*")
+    .range(1000,1500);
+
+  const { data: products4, error: error4 } = await supabase
+    .from("crutchfield")
+    .select("*")
+    .range(1500,2000);
+
+  const { data: products5, error: error5 } = await supabase
+    .from("crutchfield")
+    .select("*")
+    .range(2000,2500);
+
+  if (!products1 || !products2 || !products3 || !products4 || !products5 || error1 || error2 || error3 || error4 || error5) {
+    return <div>There was an error fetching products. {error1?.message} {error2?.message} {error3?.message} {error4?.message} {error5?.message}</div>;
   }
+
+  const products = [...products1, ...products2, ...products3, ...products4, ...products5];
 
   return <ClientSideProductGrid products={products} />;
 }
